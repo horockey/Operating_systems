bb567e87738d45524cd11d7d289e67c6317041b3
Horockey <rassudov99@gmail.com>
2022-09-20 18:02:14 +0300

    perf: exceptions handling

--- OperSysLab2/OperSysLab2/main.cpp
+++ OperSysLab2/OperSysLab2/main.cpp
@@ -3,12 +3,6 @@
 using namespace std;
 
 int main() {
-	/*auto mm = MemoryManager(1024);
-	mm.printStats();
-	mm.setByte(14, 120);
-	mm.getByte(720);
-	mm.printStats();
-	cout << (int)(mm.getByte(14)) << endl;*/
 	auto mm = MemoryManager(1024);
 	int choise = 0;
 	while (choise != 4) {
@@ -17,30 +11,36 @@ int main() {
 		cout << "3. Show RAM stats" << endl;
 		cout << "4. Exit" << endl;
 
-		cin >> choise;
-		if (choise == 1) {
-			int index, val;
-			cout << "Index: ";
-			cin >> index;
-			cout << "Value: ";
-			cin >> val;
-			mm.setByte(index, val);
+		try {
+			cin >> choise;
+			if (choise == 1) {
+				int index, val;
+				cout << "Index: ";
+				cin >> index;
+				cout << "Value: ";
+				cin >> val;
+				mm.setByte(index, val);
+			}
+			else if (choise == 2) {
+				int index;
+				cout << "Index: ";
+				cin >> index;
+				cout << (int)(mm.getByte(index)) << endl;
+			}
+			else if (choise == 3) {
+				mm.printStats();
+			}
+			else if (choise == 4) {
+				break;
+			}
+			else {
+				cout << "Invalid command!";
+			}
 		}
-		else if (choise == 2) {
-			int index;
-			cout << "Index: ";
-			cin >> index;
-			cout << (int)(mm.getByte(index)) << endl;
+		catch (exception e) {
+			cout << e.what() << endl;
 		}
-		else if (choise == 3) {
-			mm.printStats();
-		}
-		else if (choise == 4) {
-			break;
-		}
-		else {
-			cout << "Invalid command!";
-		}
+		
 	}
 	return 0;
 }
