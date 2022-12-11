e3b329cb6ec04dc2af23a1bba77557a14a8dd8ab
Horockey <rassudov99@gmail.com>
2022-12-11 21:52:33 +0300

    build: lab_3 staff

---
+++ Temp/Temp/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	cout << "Process_3" << endl;
+	cout << "Enter any value to terminate...";
+	string c;
+	cin >> c;
+	cout << "Process_3 terminated!" << endl;
+}
